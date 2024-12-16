e9346df3c1a8cee15f2bb8f93a6f3c4d537ac76c
Iris Wiscaso <iriswiscaso@Iriss-MacBook-Pro.local>
2024-12-16 10:10:59 -0600

    working_merchant_profile

--- deliver/merchant/views.py
+++ deliver/merchant/views.py
@@ -10,7 +10,7 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
     def get(self, request, *args, **kwargs):
         # get the current date 
         today=datetime.today()
-        orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
+        orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day__lte=today.day)
 
         # loop through the orders and add the price values 
         total_revenue = 0
@@ -24,6 +24,27 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
             'total_orders': len(orders)
         }
         return render(request, 'merchant/dashboard.html', context)
+    
+    def test_func(self):
+        # validate if the users are allowed to view the dashbaord or not for user passes
+        return self.request.user.groups.filter(name='Staff').exists()
+    
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order
+        }
+        return render(request, 'merchant/order_details.html', context)
+    
+    def post(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        order.ready_for_pickup=True 
+        order.save()
+
+        context = {'order': order}
+
+        return render(request, 'merchant/order_details.html', context )
     
     def test_func(self):
         # validate if the users are allowed to view the dashbaord or not for user passes
