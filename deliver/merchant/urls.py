5a10edf8338907de8470fb9e3216701c5693ea11
Iris Wiscaso <iriswiscaso@Iriss-MacBook-Pro.local>
2025-01-06 13:21:19 -0600

    implement_menu_edit

--- deliver/merchant/urls.py
+++ deliver/merchant/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import Dashboard, OrderDetails, MerchantDashboard, AddCategoryView
+from .views import Dashboard, OrderDetails, MerchantDashboard, AddCategoryView, DeleteMenuItem, ChangeItemStatusView
 app_name = 'merchant'
 urlpatterns = [
     path('merchant_dashboard/', MerchantDashboard.as_view(), name='merchant_dashboard'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'), 
     path('orders/<int:pk>', OrderDetails.as_view(), name='order-details'), 
-    path('add_category/', AddCategoryView.as_view(), name = 'add_category')
+    path('add_category/', AddCategoryView.as_view(), name = 'add_category'), 
+    path('menu-item/<int:item_id>/delete/', DeleteMenuItem.as_view(), name='delete_menu_item'),
+    path('menu-item/<int:item_id>/change-status/', ChangeItemStatusView.as_view(), name='change_item_status'),
+
 ]
